1b3a6ca952e2e4128f765bdd731d0c94ac329653
Oleg833 <oleg.kovtun447@gmail.com>
2023-03-09 05:44:52 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 // npm i notiflix
-import debounce from 'lodash.debounce';
+// import debounce from 'lodash.debounce';
 // npm i --save lodash.debounce
 // import throttle from 'lodash.throttle';
-// import axios from 'axios';
+import axios from 'axios';
 // npm install axios
 
 import pixabeyImage from './pixabeyImage';
@@ -15,8 +15,74 @@ const searchForm = document.querySelector('#search-form');
 const inputEl = document.querySelector('input');
 const buttonFormEl = document.querySelector('button');
 const loadMoreBtn = document.querySelector('.load-more');
+const galleryEl = document.querySelector('.gallery');
 
 searchForm.addEventListener('submit', onSubmitForm);
 // searchForm.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
-searchForm.addEventListener('input', onFormInput);
-loadMoreBtn.addEventListener('click', onLoadMoreClick);
+// searchForm.addEventListener('input', onFormInput);
+// loadMoreBtn.addEventListener('click', onLoadMoreClick);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const valueSearchQuery =
+    event.currentTarget.elements.searchQuery.value.trim();
+  console.log(valueSearchQuery);
+
+  // pixabeyImage('peru').then(users => console.log(users));
+
+  // pixabeyImage(valueSearchQuery)
+  //   .then(users => {
+  //     if (users.length > 20) {
+  //       clearElements();
+  //       Notiflix.Notify.info(
+  //         'Too many matches found. Please enter a more specific name.'
+  //       );
+  //       return users;
+  //     }
+  //     if (users.length === 1) {
+  //       countryList.innerHTML = '';
+  //       renderUserList(users);
+  //       return users;
+  //     }
+  //     countryInfo.innerHTML = '';
+  //     fastRender(users);
+  //     console.log(`Found:`, users.length, `country`);
+  //     return users;
+  //   })
+  //   .then(res => {
+  //     console.log(`Found Country[0]`, res[0]);
+  //     console.log(
+  //       `Country name:`,
+  //       res[0].name.official,
+  //       ` Capital:`,
+  //       Object.values(res[0].capital).join(', '),
+  //       ` Population:`,
+  //       res[0].population,
+  //       ` languages:`,
+  //       Object.values(res[0].languages).join(', ')
+  //     );
+  //   })
+  //   .catch(error => {
+  //     clearElements();
+  //     Notiflix.Notify.failure(
+  //       'Sorry, there are no images matching your search query. Please try again.'
+  //     );
+  //   });
+
+  // event.currentTarget.reset();
+}
+
+pixabeyImage('peru')
+  .then(users => console.log(users))
+  .catch(error => {
+    clearElements();
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  });
+
+function clearElements() {
+  console.log(`Brr`);
+  galleryEl.innerHTML = '';
+}
--- src/pixabeyImage.js
+++ src/pixabeyImage.js
@@ -6,18 +6,18 @@ async function pixabeyImage(param) {
   const BASE_URL = `https://restcountries.com/v3.1/name/${param}?fields=name,capital,population,flags,languages`;
   const PIXA_URL = `https://pixabay.com/api/?key=34212854-f6457ae4e5e1013dd0f507693&q=${param}&image_type=photo&orientation=horizontal&safesearch=true`;
   const PIXA_ORIG = `https://pixabay.com/api/?key=34212854-f6457ae4e5e1013dd0f507693&q=yellow+flowers&image_type=photo&pretty=true`;
-  const sfsd = {
-    webformatURL,
-    largeImageURL,
-    tags,
-    likes,
-    views,
-    comments,
-    downloads,
-  };
+  //   const sfsd = {
+  //     webformatURL,
+  //     largeImageURL,
+  //     tags,
+  //     likes,
+  //     views,
+  //     comments,
+  //     downloads,
+  //   };
 
   try {
-    const response = await axios.get(PIXA_URL);
+    const response = await axios.get(BASE_URL);
     console.log(response.data);
     return response.data;
   } catch (error) {
