f82b6b0ff32c0a6b106e83fcfd4036feda843218
Oleg833 <oleg.kovtun447@gmail.com>
2023-03-13 10:39:55 +0200

    cardHeight

--- src/index.js
+++ src/index.js
@@ -117,6 +117,15 @@ function renderGallery(users) {
   galleryContainer.insertAdjacentHTML('beforeend', gallaryMarkup);
 
   gallery.refresh();
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
 
 let gallery = new SimpleLightbox('.gallery a', {
